854302c675cb4b3e35b59ee32ad134322a0a1060
Pavel <jronima8@gmail.com>
2026-02-25 14:30:53 +0500

    fixed wa.me

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -7,7 +7,7 @@ export default function Socials() {
       <a href="https://t.me/PakPavelM" target="_blank" rel="noreferrer" aria-label="Telegram" className="text-emerald-300 hover:text-emerald-200 p-1.5 sm:p-0">
         <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M21.4 4.6c-.2-.2-.6-.3-.9-.2L3.1 11.2c-.5.2-.8.7-.7 1.2.1.5.5.9 1 .9l4.5.9 1.7 5.2c.1.4.5.7.9.8h.1c.4 0 .7-.2.9-.5l2.5-3.2 4.2 3.1c.3.2.7.3 1 .1.3-.1.6-.4.6-.8l2.1-13.2c.1-.4 0-.8-.3-1zM9.5 13.8l8.3-6.1-6.8 7.2-.4 2.5-1.1-3.6z"/></svg>
       </a>
-      <a href="https://wa.me/87772299744" target="_blank" rel="noreferrer" aria-label="WhatsApp" className="text-emerald-300 hover:text-emerald-200 p-1.5 sm:p-0">
+      <a href="https://wa.me/+77772299744" target="_blank" rel="noreferrer" aria-label="WhatsApp" className="text-emerald-300 hover:text-emerald-200 p-1.5 sm:p-0">
         <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M20.5 3.5A11.8 11.8 0 0 0 12 0C5.4 0 0 5.4 0 12c0 2.1.5 4.1 1.5 5.9L0 24l6.3-1.6A12 12 0 0 0 12 24c6.6 0 12-5.4 12-12 0-3.2-1.3-6.2-3.5-8.5zM12 21.8c-1.8 0-3.5-.5-5-1.4l-.4-.2-3.7 1 .9-3.6-.2-.4A9.6 9.6 0 1 1 12 21.8zm5.3-7.2c-.3-.2-1.8-.9-2.1-1-.3-.1-.5-.2-.7.2-.2.3-.8 1-1 1.2-.2.2-.4.2-.7.1-.3-.2-1.4-.5-2.6-1.7-1-1-1.6-2.1-1.8-2.5-.2-.3 0-.5.1-.7.1-.1.3-.3.4-.5.1-.2.2-.3.3-.5.1-.2 0-.4 0-.5 0-.1-.7-1.7-1-2.3-.2-.5-.5-.4-.7-.4h-.6c-.2 0-.5.1-.8.4-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.8 4.3.7.3 1.3.5 1.7.6.7.2 1.4.2 1.9.1.6-.1 1.8-.7 2.1-1.3.3-.6.3-1.2.2-1.3 0-.1-.2-.2-.5-.4z"/></svg>
       </a>
       <a href="mailto:jronima8@gmail.com" aria-label="Email" className="text-emerald-300 hover:text-emerald-200 p-1.5 sm:p-0">
